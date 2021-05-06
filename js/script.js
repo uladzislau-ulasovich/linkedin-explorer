354cb176458a263758836bbdc5fbc47d38db15a7
Limonische <gulisnikita@gmail.com>
2021-05-06 19:03:18 +0300

    Add createElementFromHTML helper

--- js/script.js
+++ js/script.js
@@ -29,6 +29,14 @@
             }, ms)
         })
 
+    const createElementFromHTML = htmlString => {
+        const template = document.createElement('template')
+
+        template.innerHTML = htmlString.trim()
+
+        return template.content.firstChild
+    }
+
     class LinkedInExplorerPlugin {
         iframe = null
         links = []
